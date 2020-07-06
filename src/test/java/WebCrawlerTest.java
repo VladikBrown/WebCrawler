a15feafe1ed0f5d3f96d5d469ad5faa84ea1f470
Vladislav Brown <vladikbrown@gmail.com>
2020-07-06 21:09:18 +0300

    initial commit

---
+++ src/test/java/WebCrawlerTest.java
@@ -0,0 +1,11 @@
+import controller.WebCrawler;
+import org.junit.jupiter.api.Test;
+
+public class WebCrawlerTest {
+    private final WebCrawler webCrawler = new WebCrawler();
+
+    @Test
+    void findAllRefsTest(){
+
+    }
+}
